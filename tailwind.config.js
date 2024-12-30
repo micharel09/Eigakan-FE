f5a6ebc5ae594773cbf4019c0b946af37d756df8
Tran Minh Quan <steammicharel@gmail.com>
2024-12-30 17:47:01 +0700

    Initial commit: Vite + React + Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}" // This line is important - it includes .jsx files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
