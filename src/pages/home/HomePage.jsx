0a37e25363d69a5c354c0bc726392a4f12098d89
Tran Minh Quan <steammicharel@gmail.com>
2025-01-08 08:04:20 +0700

    set up sign up/in, AuthScreen

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,16 @@
+import AuthScreen from "./AuthScreen"
+import HomeScreen from "./HomeScreen"
+import React from 'react'
+
+
+const HomePage = () => {
+
+  const user = false;
+  return (
+    <div >
+      {user ? <HomeScreen/> : <AuthScreen />}
+    </div>
+  )
+}
+
+export default HomePage
