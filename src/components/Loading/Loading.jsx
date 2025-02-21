35fcb42cd5f94f6a3d4e56031d2930a65b6e0413
Tran Minh Quan <97839306+micharel09@users.noreply.github.com>
2025-02-21 15:03:05 +0700

    fix: news management create, delete duplicate layouts

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Spin } from "antd";
 
-const Loading = () => {
+const Loading = ({ isVisible = true }) => {
+  if (!isVisible) return null;
+
   return (
     <div className="flex justify-center items-center h-screen bg-gray-900">
       <Spin size="large" style={{ color: "#FF009F" }} />
